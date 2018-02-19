7f8fa8ccce2348d25fd48a1121b80030bfb50975
Osuma <40872327@Americas.OneACS.com>
2018-02-19 07:46:10 -0500

    Bookstore API Project

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app =  express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.json);
+
+Genre = require('./models/genre');
+Book = require('./models/book');
+
+//Connect to Mongoose
+mongoose.connect('mongodb://localhost/bookstore')
+var db = mongoose.connection;
+
+app.get('/', function(req, res){
+    res.send('Please use /api/boks or api/geners');
+});
+    
+app.get('/api/genres', function(req,res){
+    Genre.getGenres(function(err, genres){
+        if(err){
+            throw err;
+        }
+        res.json(genres);  
+    });
+});
+
+app.post('/api/genres', function(req,res){
+    var genre = req.body;
+    
+    Genre.addGenre(genre, function(err, genre){
+        if(err){
+            throw err;
+        }
+        res.json(genre);  
+    });
+});
+
+app.get('/api/books', function(req,res){
+    Book.getBooks(function(err, books){
+        if(err){
+            throw err;
+        }
+        res.json(books);  
+    });
+});
+
+app.get('/api/books/:_id', function(req,res){
+    Book.getBookById(req.params._id, function(err, book){
+        if(err){
+            throw err;
+        }
+        res.json(book);  
+    });
+});
+    
+app.listen(4000);
+console.log('Server started on port 4000...');
